c64a262a9faa285bb3f46d7a382e4bed13185126
wjlhhh <15101003835@163.com>
2018-06-21 21:31:38 +0800

    sec test

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/

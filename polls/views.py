443148657a26b1371770d9a47415ed0387395423
wjlhhh <15101003835@163.com>
2018-06-22 11:27:13 +0800

    company tx

--- polls/views.py
+++ polls/views.py
@@ -5,15 +5,17 @@ from .models import Question,Choice
 from django.template import loader
 from django.views import generic
 
+from django.utils import timezone
+
 # Create your views here
 
-
+"""
 def index(request):
     #latest_question_list = Question.objects.order_by('-pub_date')
     latest_question_list = Question.objects.all()
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-"""
+
 def detail(request, question_id):
     '''
     try:
@@ -46,7 +48,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
+"""
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -54,8 +56,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         #Return the last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
-"""
+        #return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    ...
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class DetailView(generic.DetailView):
     model = Question
